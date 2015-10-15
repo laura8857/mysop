b362285aeba6d1f12e8fa315d175158c00e48178
KellyHsu <kelly830619@gmail.com>
2015-10-15 16:50:41 +0800

    1015
    orm
    User
    DatabaseHelper

---
+++ app/src/main/java/com/example/kelly/mysop/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.kelly.mysop;
+
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.GenericRawResults;
+import com.j256.ormlite.dao.RawRowMapper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Demo";
+    private static DatabaseHelper instance;
+
+
+    private DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static synchronized DatabaseHelper getHelper(Context context) {
+        context = context.getApplicationContext();
+        if (instance == null) {
+            synchronized (DatabaseHelper.class) {
+                if (instance == null)
+                    instance = new DatabaseHelper(context);
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, User.class, true);
+            //TableUtils.dropTable(connectionSource, Group.class, true);
+            onCreate(database, connectionSource);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, User.class);
+            //TableUtils.createTable(connectionSource, Group.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
