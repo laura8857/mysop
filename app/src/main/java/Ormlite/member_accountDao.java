5a9bef54f2891d7f7ac68a82d68cac45e6fc008e
tina8857 <tina8857@yahoo.com.tw>
2015-10-27 12:23:16 +0800

    00

--- app/src/main/java/Ormlite/member_accountDao.java
+++ app/src/main/java/Ormlite/member_accountDao.java
@@ -4,139 +4,105 @@ import java.sql.SQLException;
 import java.util.List;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 //import com.ck.ap.DatabaseHelper;
 
 //�ccount Dao��
-public class member_accountDao
-{
+public class member_accountDao {
+
+
 	/* insert */
-	public static int insert(ConnectionSource connectionSource, member_accountVo member_accountVo)
-	{
-		try
-		{
-			Dao<member_accountVo, Integer> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
-			if (exist(connectionSource, member_accountVo))
-			{
-				return 0;
-			}
-			return member_accountDao.create(member_accountVo);
+	public static int insert(DatabaseHelper databaseHelper, member_accountVo member_accountVo) {
+		RuntimeExceptionDao<member_accountVo, Integer> accountDao = databaseHelper.getMember_accountDao();
+		if (exist(databaseHelper, member_accountVo)) {
+			return 0;
 		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return 0;
+		return accountDao.create(member_accountVo);
 	}
 
 	/* exist */
-	public static boolean exist(ConnectionSource connectionSource, member_accountVo member_accountVo)
-	{
-		try
-		{
+	public static boolean exist(DatabaseHelper databaseHelper, member_accountVo member_accountVo) {
+		try {
 			Dao<member_accountVo, Integer> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
 			QueryBuilder<member_accountVo, Integer> queryBuilder = member_accountDao.queryBuilder();
 
-			boolean ret=false;
+			boolean ret = false;
 			//�撣唾����
-			queryBuilder.where().eq(member_accountVo.FIELD_Account,member_accountVo.getAccount());
+			queryBuilder.where().eq(member_accountVo.FIELD_Account, member_accountVo.getAccount());
 			ret = queryBuilder.query().size() > 0 ? true : false;
 
 			return ret;
-		}
-		catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
 	/* update */
-	public static int update(ConnectionSource connectionSource, member_accountVo member_accountVo)
-	{
-		try
-		{
+	public static int update(ConnectionSource connectionSource, member_accountVo member_accountVo) {
+		try {
 			Dao<member_accountVo, Integer> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
 			return member_accountDao.update(member_accountVo);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return 0;
 	}
 
 	/* delete */
-	public static int delete(ConnectionSource connectionSource, member_accountVo member_accountVo)
-	{
-		try
-		{
+	public static int delete(ConnectionSource connectionSource, member_accountVo member_accountVo) {
+		try {
 			Dao<member_accountVo, Integer> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
 			return member_accountDao.delete(member_accountVo);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return 0;
 	}
 
 	/* select by id */
-	public static member_accountVo select(ConnectionSource connectionSource, int id)
-	{
-		try
-		{
+	public static member_accountVo select(ConnectionSource connectionSource, int id) {
+		try {
 			Dao<member_accountVo, Integer> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
 			return member_accountDao.queryForId(id);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
 	/* selectRaw */
-	public static List<member_accountVo> selectRaw(ConnectionSource connectionSource, String rawWhere)
-	{
-		try
-		{
+	public static List<member_accountVo> selectRaw(ConnectionSource connectionSource, String rawWhere) {
+		try {
 			Dao<member_accountVo, String> member_accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
 			QueryBuilder<member_accountVo, String> queryBuilder = member_accountDao.queryBuilder();
 			queryBuilder.where().raw(rawWhere);
 			String sql = queryBuilder.prepareStatementString();
 			return queryBuilder.query();
-		}
-		catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+
 	// 頛����摰惜蝝���
-	public static member_accountVo getMember_accountVo(ConnectionSource connectionSource)
-	{
-		try
-		{
-			Dao<member_accountVo, Integer> accountDao = DaoManager.createDao(connectionSource, member_accountVo.class);
+	public static member_accountVo getMember_accountVo(DatabaseHelper databaseHelper) {
+		try {
+			Dao<member_accountVo, Integer> accountDao = databaseHelper.getDao(member_accountVo.class);
 			List<member_accountVo> data = accountDao.queryForAll();
 
-			if (data.size() > 0)
-			{
+			if (data.size() > 0) {
 				return data.get(0);
-			}
-			else
-			{
+			} else {
 				// 瘝����
 				// AccountVo aAccountVo = new AccountVo();
 				// AccountDao.insert(databaseHelper, aAccountVo);
 				return null;
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
