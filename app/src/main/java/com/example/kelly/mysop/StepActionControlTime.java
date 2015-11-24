05efd24c10c8f3d767e03aab37b657a0769e0a7d
tina8857 <tina8857@yahoo.com.tw>
2015-11-24 18:58:38 +0800

    ACtime

--- app/src/main/java/com/example/kelly/mysop/StepActionControlTime.java
+++ app/src/main/java/com/example/kelly/mysop/StepActionControlTime.java
@@ -5,23 +5,24 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import Ormlite.DatabaseHelper;
+import Ormlite.sop_detailDao;
+import Ormlite.sop_detailVo;
 
 
 public class StepActionControlTime extends Activity {
@@ -34,12 +35,16 @@ public class StepActionControlTime extends Activity {
     private static Button timebtn;
     private static String Starttime="";
     private static TextView timedifference;
-    //檢查 是否有過期 0未過期 1過期 2剛好
+    //檢查 是否有過期 0未過期 1過期
     private static int check=0;
 
     String TAG_CASE_NUMBER = "";
     String TAG_STEP_NUMBER = "";
     int TAG_STEP_ORDER = 0;
+
+    //orm
+    private RuntimeExceptionDao<sop_detailVo, Integer> sop_detailRuntimeDao;
+    private sop_detailDao msop_detailDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +67,88 @@ public class StepActionControlTime extends Activity {
         timebtn=(Button)findViewById(R.id.timecheck);
         timedifference=(TextView)findViewById(R.id.timedifference);
 
-        new CheckTime().execute();
+        //orm 用stepnumber去抓資料庫的東西
+        msop_detailDao = new sop_detailDao();
+        DatabaseHelper mDatabaseHelper = DatabaseHelper.getHelper(this);
+        List<sop_detailVo> list = null;
+        list = msop_detailDao.selectRaw(mDatabaseHelper, "Step_number =" + TAG_STEP_NUMBER);
+        Log.d("抓", list.get(0).getStart_value1());
+        Starttime=list.get(0).getStart_value1();
+
+        if(str.equals(Starttime)){
+            Intent intent1 = new Intent(StepActionControlTime.this,Stepdescription.class);
+            Bundle bundle1 = new Bundle();
+            bundle1.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
+            bundle1.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
+            bundle1.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
+            intent1.putExtras(bundle1);//將參數放入intent
+            startActivity(intent1);
+            timebtn.setText("啟動");
+
+        }else {
+            SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
+            Date now = null;
+
+            try {
+                now = df.parse(Starttime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Date date = null;
+            try {
+                date = df.parse(str);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            long l;
+
+            //比較時間大小
+            if(now.getTime()>date.getTime()) {
+                //未過期
+                l = now.getTime() - date.getTime();
+                check=0;
+            }else{
+                //過期
+                l = date.getTime() - now.getTime();
+                check=1;
+            }
+            long l2 = l/(30*24*60*60);
+
+            //計算時間差
+            long month=l2/1000;
+            long day = l / (24 * 60 * 60 * 1000)- month * 30;
+            long hour = (l / (60 * 60 * 1000) - month * 30 * 24 - day * 24);
+            long min = ((l / (60 * 1000)) - month * 30 * 24 * 60- day * 24 * 60 - hour * 60);
+            long s = (l / 1000 - month * 30 * 24 * 60 * 60- day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+            System.out.println(month+"月" + day + "天" + hour + "小时" + min + "分" + s + "秒");
+
+            if(check==0) {
+                timebtn.setText("時辰未到");
+                if (month == 0) {
+                    timedifference.setText("還差" + day + "天" + hour + "小时" + min + "分");
+                } else if (month == 0 && day == 0) {
+                    timedifference.setText("還差" + hour + "小时" + min + "分");
+                } else if (month == 0 && day == 0 && hour == 0) {
+                    timedifference.setText("還差" + min + "分");
+                    timedifference.setTextColor(Color.RED);
+                } else {
+                    timedifference.setText("還差" + month + "月" + day + "天" + hour + "小时" + min + "分");
+                }
+            }else{
+                //過期
+                Intent intent2 = new Intent(StepActionControlTime.this,Stepdescription.class);
+                Bundle bundle2 = new Bundle();
+                bundle2.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
+                bundle2.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
+                bundle2.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
+                intent2.putExtras(bundle2);//將參數放入intent
+                startActivity(intent2);
+                timebtn.setText("啟動");
+            }
+        }
+
+
+       // new CheckTime().execute();
     }
 
 
@@ -111,123 +197,123 @@ public class StepActionControlTime extends Activity {
         }
     }
 
-    class CheckTime extends AsyncTask<String, String, Integer> {
-
-
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(StepActionControlTime.this);
-            pDialog.setMessage("Time checking.... Please wait...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
-        }
-
-        protected Integer doInBackground(String... args) {
-            //String Stepnumber="5";
-            String Stepnumber = TAG_STEP_NUMBER;
-            String Time= str;
-            Integer CHECK=0;//CHECK 0代表過期或未到 1代表已到
-
-            ArrayList params = new ArrayList();
-            params.add(new BasicNameValuePair("Stepnumber", Stepnumber));
-            params.add(new BasicNameValuePair("Time", Time));
-
-            JSONObject json = StepActionControlTime.this.jsonParser.makeHttpRequest(StepActionControlTime.url_create_product, "POST", params);
-
-
-            try {
-                int e = json.getInt(TAG_SUCCESS);
-                if(e == 1) {
-                    Intent intent = new Intent(StepActionControlTime.this,Stepdescription.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
-                    bundle.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
-                    bundle.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
-                    intent.putExtras(bundle);//將參數放入intent
-                    startActivity(intent);
-                    CHECK=1;
-                    timebtn.setText("啟動");
-
-                }else if(e == 6){
-
-                    Starttime=json.getString("starttime");
-                    System.out.println("Startime"+Starttime);
-
-                }
-            } catch (JSONException var9) {
-                var9.printStackTrace();
-            }
-
-            return CHECK;
-        }
-
-        protected void onPostExecute(Integer CHECK) {
-
-            pDialog.dismiss();
-            //計算時間差並顯示
-            if(CHECK==0) {
-                SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
-                Date now = null;
-
-                try {
-                    now = df.parse(Starttime);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                Date date = null;
-                try {
-                    date = df.parse(str);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                long l;
-
-                //比較時間大小
-                if(now.getTime()>date.getTime()) {
-                    //未過期
-                     l = now.getTime() - date.getTime();
-                    check=0;
-                }else{
-                    //過期
-                    l = date.getTime() - now.getTime();
-                    check=1;
-                }
-                long l2 = l/(30*24*60*60);
-
-                //計算時間差
-                long month=l2/1000;
-                long day = l / (24 * 60 * 60 * 1000)- month * 30;
-                long hour = (l / (60 * 60 * 1000) - month * 30 * 24 - day * 24);
-                long min = ((l / (60 * 1000)) - month * 30 * 24 * 60- day * 24 * 60 - hour * 60);
-                long s = (l / 1000 - month * 30 * 24 * 60 * 60- day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
-                System.out.println(month+"月" + day + "天" + hour + "小时" + min + "分" + s + "秒");
-
-                if(check==0) {
-                    timebtn.setText("時辰未到");
-                    if (month == 0) {
-                        timedifference.setText("還差" + day + "天" + hour + "小时" + min + "分");
-                    } else if (month == 0 && day == 0) {
-                        timedifference.setText("還差" + hour + "小时" + min + "分");
-                    } else if (month == 0 && day == 0 && hour == 0) {
-                        timedifference.setText("還差" + min + "分");
-                        timedifference.setTextColor(Color.RED);
-                    } else {
-                        timedifference.setText("還差" + month + "月" + day + "天" + hour + "小时" + min + "分");
-                    }
-                }else{
-                    //過期
-                    Intent intent = new Intent(StepActionControlTime.this,Stepdescription.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
-                    bundle.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
-                    bundle.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
-                    intent.putExtras(bundle);//將參數放入intent
-                    startActivity(intent);
-                    timebtn.setText("啟動");
-                }
-            }
-
-        }
-    }
+//    class CheckTime extends AsyncTask<String, String, Integer> {
+//
+//
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            pDialog = new ProgressDialog(StepActionControlTime.this);
+//            pDialog.setMessage("Time checking.... Please wait...");
+//            pDialog.setIndeterminate(false);
+//            pDialog.setCancelable(false);
+//            pDialog.show();
+//        }
+//
+//        protected Integer doInBackground(String... args) {
+//            //String Stepnumber="5";
+//            String Stepnumber = TAG_STEP_NUMBER;
+//            String Time= str;
+//            Integer CHECK=0;//CHECK 0代表過期或未到 1代表已到
+//
+//            ArrayList params = new ArrayList();
+//            params.add(new BasicNameValuePair("Stepnumber", Stepnumber));
+//            params.add(new BasicNameValuePair("Time", Time));
+//
+//            JSONObject json = StepActionControlTime.this.jsonParser.makeHttpRequest(StepActionControlTime.url_create_product, "POST", params);
+//
+//
+//            try {
+//                int e = json.getInt(TAG_SUCCESS);
+//                if(e == 1) {
+//                    Intent intent = new Intent(StepActionControlTime.this,Stepdescription.class);
+//                    Bundle bundle = new Bundle();
+//                    bundle.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
+//                    bundle.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
+//                    bundle.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
+//                    intent.putExtras(bundle);//將參數放入intent
+//                    startActivity(intent);
+//                    CHECK=1;
+//                    timebtn.setText("啟動");
+//
+//                }else if(e == 6){
+//
+//                    Starttime=json.getString("starttime");
+//                    System.out.println("Startime"+Starttime);
+//
+//                }
+//            } catch (JSONException var9) {
+//                var9.printStackTrace();
+//            }
+//
+//            return CHECK;
+//        }
+//
+//        protected void onPostExecute(Integer CHECK) {
+//
+//            pDialog.dismiss();
+//            //計算時間差並顯示
+//            if(CHECK==0) {
+//                SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
+//                Date now = null;
+//
+//                try {
+//                    now = df.parse(Starttime);
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+//                Date date = null;
+//                try {
+//                    date = df.parse(str);
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+//                long l;
+//
+//                //比較時間大小
+//                if(now.getTime()>date.getTime()) {
+//                    //未過期
+//                     l = now.getTime() - date.getTime();
+//                    check=0;
+//                }else{
+//                    //過期
+//                    l = date.getTime() - now.getTime();
+//                    check=1;
+//                }
+//                long l2 = l/(30*24*60*60);
+//
+//                //計算時間差
+//                long month=l2/1000;
+//                long day = l / (24 * 60 * 60 * 1000)- month * 30;
+//                long hour = (l / (60 * 60 * 1000) - month * 30 * 24 - day * 24);
+//                long min = ((l / (60 * 1000)) - month * 30 * 24 * 60- day * 24 * 60 - hour * 60);
+//                long s = (l / 1000 - month * 30 * 24 * 60 * 60- day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+//                System.out.println(month+"月" + day + "天" + hour + "小时" + min + "分" + s + "秒");
+//
+//                if(check==0) {
+//                    timebtn.setText("時辰未到");
+//                    if (month == 0) {
+//                        timedifference.setText("還差" + day + "天" + hour + "小时" + min + "分");
+//                    } else if (month == 0 && day == 0) {
+//                        timedifference.setText("還差" + hour + "小时" + min + "分");
+//                    } else if (month == 0 && day == 0 && hour == 0) {
+//                        timedifference.setText("還差" + min + "分");
+//                        timedifference.setTextColor(Color.RED);
+//                    } else {
+//                        timedifference.setText("還差" + month + "月" + day + "天" + hour + "小时" + min + "分");
+//                    }
+//                }else{
+//                    //過期
+//                    Intent intent = new Intent(StepActionControlTime.this,Stepdescription.class);
+//                    Bundle bundle = new Bundle();
+//                    bundle.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
+//                    bundle.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
+//                    bundle.putInt("TAG_STEP_ORDER", TAG_STEP_ORDER);
+//                    intent.putExtras(bundle);//將參數放入intent
+//                    startActivity(intent);
+//                    timebtn.setText("啟動");
+//                }
+//            }
+//
+//        }
+//    }
 }
