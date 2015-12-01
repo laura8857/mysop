bad78b55914a1b4ea5511fdd039a0e0a50b4ace4
tina8857 <tina8857@yahoo.com.tw>
2015-12-01 16:43:05 +0800

    1201
    修修宇凡的教的

--- app/src/main/java/Ormlite/sop_masterDao.java
+++ app/src/main/java/Ormlite/sop_masterDao.java
@@ -1,12 +1,10 @@
 package Ormlite;
 
-import java.sql.SQLException;
-import java.util.List;
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.j256.ormlite.stmt.QueryBuilder;
-import com.j256.ormlite.support.ConnectionSource;
+
+import java.sql.SQLException;
+import java.util.List;
 //import com.ck.ap.DatabaseHelper;
 
 //【sop_master Dao】
@@ -104,6 +102,30 @@ public class sop_masterDao
                 .queryBuilder();
         try {
             queryBuilder.where().raw(rawWhere);
+            return queryBuilder.query();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /* selectRawByNest */
+    public static List<sop_masterVo> selectRawByNest(DatabaseHelper databaseHelper,String column1,String value1,String column2) {
+        RuntimeExceptionDao<sop_masterVo, Integer> sop_masterDao = databaseHelper
+                .getSop_masterDao();
+        RuntimeExceptionDao<case_masterVo, Integer> case_masterDao = databaseHelper
+                .getCase_masterDao();
+        QueryBuilder<case_masterVo, Integer> subqueryBuilder = case_masterDao
+                .queryBuilder();
+        QueryBuilder<sop_masterVo, Integer> queryBuilder = sop_masterDao
+                .queryBuilder();
+        try {
+            subqueryBuilder.where().eq(column1,value1);
+            //Log.d("TEST NEST",subqueryBuilder.query().get(0).getAccount());
+            // in using the sub-query
+            subqueryBuilder.selectColumns(column2);
+            queryBuilder.where().in(column2, subqueryBuilder);
+            //Log.d("TEST NEST",queryBuilder.query().get(0).getAccount());
             return queryBuilder.query();
         } catch (SQLException e) {
             e.printStackTrace();
