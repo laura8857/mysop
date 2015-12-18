0208a8912a18cb49b5d5a54f85d75439c21e252c
KellyHsu <kelly830619@gmail.com>
2015-12-18 19:22:07 +0800

    1218

--- app/src/main/java/com/example/kelly/mysop/Register.java
+++ app/src/main/java/com/example/kelly/mysop/Register.java
@@ -173,8 +173,8 @@ public class Register extends Activity {
                 int e = json.getInt(TAG_SUCCESS);
                 if(e == 1) {
                     Register.this.TAG_ACCOUNT = Register.this.et1.getText().toString();
-                    //Intent i = new Intent(Register.this.getApplicationContext(), Emailverify.class);
-                   Intent i = new Intent(Register.this.getApplicationContext(), Search.class);
+                    Intent i = new Intent(Register.this.getApplicationContext(), Emailverify.class);
+                   //Intent i = new Intent(Register.this.getApplicationContext(), Search.class);
 
                     //設定傳送參數
                     Bundle bundle = new Bundle();
