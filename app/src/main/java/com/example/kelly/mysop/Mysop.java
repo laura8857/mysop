2809f9bba6d2a3be073054635337b23979d1bfed
Chris <cliff5345179@gmail.com>
2015-12-24 22:40:05 +0800

    1224

--- app/src/main/java/com/example/kelly/mysop/Mysop.java
+++ app/src/main/java/com/example/kelly/mysop/Mysop.java
@@ -1,5 +1,9 @@
 package com.example.kelly.mysop;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -43,7 +47,7 @@ import Ormlite.sop_detailVo;
 import Ormlite.sop_masterDao;
 import Ormlite.sop_masterVo;
 
-public class Mysop extends Activity {
+public class Mysop extends ActionBarActivity {
 
 
 
