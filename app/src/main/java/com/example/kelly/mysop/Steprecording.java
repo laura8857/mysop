a11258ebe050cdd782fc081d03604136ad37d17f
KellyHsu <kelly830619@gmail.com>
2016-01-14 18:44:17 +0800

    0114
    我不會qaq

--- app/src/main/java/com/example/kelly/mysop/Steprecording.java
+++ app/src/main/java/com/example/kelly/mysop/Steprecording.java
@@ -21,6 +21,14 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+import java.text.DateFormat;
+
 import org.json.JSONArray;
 
 import java.util.ArrayList;
@@ -73,6 +81,14 @@ public class Steprecording extends Activity {
     private case_recordDao mcase_recordDao;
     private case_recordVo mcase_recordVo;
 
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "PhoneActivity";
+    public static final String NOTIFICATION_PATH = "/notification";
+    public static final String NOTIFICATION_TIMESTAMP = "timestamp";
+    public static final String NOTIFICATION_TITLE = "title";
+    public static final String NOTIFICATION_CONTENT = "content";
+
+    public static final String ACTION_DISMISS = "com.example.kelly.mysop.DISMISS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -137,6 +153,48 @@ public class Steprecording extends Activity {
         //llb.setOnTouchListener(new MyOnTouchListener());
 
         messageList = new ArrayList<>();
+
+        //手錶
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        Log.d(TAG, "onConnected: " + connectionHint);
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        Log.d(TAG, "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.d(TAG, "onConnectionFailed: " + result);
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+        sendNotification();
+    }
+
+    //手錶
+    private void sendNotification() {
+        if (mGoogleApiClient.isConnected()) {
+            PutDataMapRequest dataMapRequest = PutDataMapRequest.create(NOTIFICATION_PATH);
+            // Make sure the data item is unique. Usually, this will not be required, as the payload
+            // (in this case the title and the content of the notification) will be different for almost all
+            // situations. However, in this example, the text and the content are always the same, so we need
+            // to disambiguate the data item by adding a field that contains teh current time in milliseconds.
+            dataMapRequest.getDataMap().putDouble(NOTIFICATION_TIMESTAMP, System.currentTimeMillis());
+            dataMapRequest.getDataMap().putString(NOTIFICATION_TITLE, "This is the title OHYA");
+            dataMapRequest.getDataMap().putString(NOTIFICATION_CONTENT, "This is a notification with some text OHYA.");
+            PutDataRequest putDataRequest = dataMapRequest.asPutDataRequest();
+            Wearable.DataApi.putDataItem(mGoogleApiClient, putDataRequest);
+        }
+        else {
+            Log.e(TAG, "No connection to wearable available!");
+        }
     }
 
 
--- wear/src/main/java/com/example/kelly/wear/RecordOnWear.java
+++ wear/src/main/java/com/example/kelly/wear/RecordOnWear.java
@@ -2,10 +2,11 @@ package com.example.kelly.wear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class RecordOnWear extends Activity {
+public class RecordOnWear extends WearableActivity {
 
     private TextView mTextView;
 
@@ -13,6 +14,8 @@ public class RecordOnWear extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_on_wear);
+        setAmbientEnabled();
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -20,5 +23,9 @@ public class RecordOnWear extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+
+
+
     }
 }
